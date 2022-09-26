b9aaec4c671bc5fe391641c5407182c3a1c8ef18
Jonathan Roesner <jonathan.roesner@espero.tech>
2022-09-26 13:46:08 +0200

    Revert "WIP"
    This reverts commit 64040b7043631e75f5d6c05f31322efc1ef5ff76.

--- client/src/context/index.tsx
+++ client/src/context/index.tsx
@@ -1,14 +1,9 @@
 import { FunctionComponent } from 'react';
 
-import CurrentRequestProvider from './CurrentRequestContext';
 import UserProvider, { UserContext } from './UserContext';
 
 const ContextProvider: FunctionComponent = ({ children }) => {
-  return (
-    <UserProvider>
-      <CurrentRequestProvider>{children}</CurrentRequestProvider>
-    </UserProvider>
-  );
+  return <UserProvider>{children}</UserProvider>;
 };
 
 export { UserContext };
