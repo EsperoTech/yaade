8fdb36b3242a4a569cef73e332f97e1a191a3fd8
Jonathan Roesner <jonathan.roesner@yahoo.com>
2024-05-22 21:17:40 +0200

    make it configurable

--- client/src/components/requestPanel/RequestSender.tsx
+++ client/src/components/requestPanel/RequestSender.tsx
@@ -240,7 +240,11 @@ function RequestSender({
           openExtModal();
           throw Error('Extension not initialized');
         }
-        response = await sendRequestToExtension(injectedReq, envName);
+        response = await sendRequestToExtension(
+          injectedReq,
+          envName,
+          collection?.data?.settings?.extensionOptions?.timeout,
+        );
         break;
       default:
         throw Error('Unknown proxy');
@@ -329,11 +333,8 @@ function RequestSender({
   async function sendRequestToExtension(
     request: Request,
     envName?: string,
-    n?: number,
+    timeout = 5,
   ): Promise<Response> {
-    if (n && n >= 5) {
-      throw Error('Exec loop detected in request script');
-    }
     return new Promise((resolve, reject) => {
       const messageId = createMessageId(request.id);
 
@@ -356,7 +357,7 @@ function RequestSender({
         // Remove the event listener if the Promise is not resolved after 5 seconds
         window.removeEventListener('message', handleMessage);
         reject(new Error('Timeout wating for response from: ' + request.id));
-      }, 5000);
+      }, timeout * 1000);
 
       // TODO: check if this mutates the original request object
       let interpolatedRequest = { ...request };
@@ -388,7 +389,6 @@ function RequestSender({
           metaData: {
             messageId,
             envName,
-            isRequestScript: n ?? 0 > 0,
           },
         },
         '*',
